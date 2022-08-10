af7b7df17053ff29b4867d433abc280c79c43acf
turgutersozmobitek <98022847+turgutersozmobitek@users.noreply.github.com>
2022-08-11 00:12:28 +0300

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,196 @@
+#bir oluşturalım 
+
+resource "vcd_vapp_org_network" "vappOrgNet" {
+  org = var.org_name
+  vdc = var.vdc_name
+  vapp_name = var.vapp_name
+
+  # Comment below line to create an isolated vApp network
+  org_network_name = var.org_network_name
+}
+
+resource "vcd_vapp_vm" "web1" {
+  vapp_name     = var.vapp_name
+  name          = var.vm_name
+  catalog_name  = var.catalog_name
+  template_name = var.template_name
+  memory        = var.vm_memory
+  cpus          = var.vm_cpu
+  cpu_cores     = var.vm_cpu_core
+  cpu_hot_add_enabled=true
+  memory_hot_add_enabled=true
+  power_on=true
+
+    override_template_disk {
+    bus_type        = "paravirtual"
+    size_in_mb      = var.vm_disk_size
+    bus_number      = 0
+    unit_number     = 0
+    iops            = 0
+  }
+
+
+  network {
+    type               = "org"
+    name               = var.org_network_name
+    ip_allocation_mode = "MANUAL"
+    ip                 = var.vm_ip
+    adapter_type       = "VMXNET3"
+    is_primary         = true
+  }
+
+   customization{
+      enabled=false
+    }
+
+
+}
+
+resource "vcd_vapp_vm" "web2" {
+  vapp_name     = var.vapp_name
+  name          = var.vm2_name
+  catalog_name  = var.catalog2_name
+  template_name = var.template2_name
+  memory        = var.vm2_memory
+  cpus          = var.vm2_cpu
+  cpu_cores     = var.vm2_cpu_core
+  cpu_hot_add_enabled=true
+  memory_hot_add_enabled=true
+  power_on=true
+
+    override_template_disk {
+    bus_type        = "paravirtual"
+    size_in_mb      = var.vm2_disk_size
+    bus_number      = 0
+    unit_number     = 0
+    iops            = 0
+  }
+
+
+  network {
+    type               = "org"
+    name               = var.org_network_name
+    ip_allocation_mode = "MANUAL"
+    ip                 = var.vm2_ip
+    adapter_type       = "VMXNET3"
+    is_primary         = true
+  }
+
+   customization{
+      enabled=false
+    }
+
+
+}
+
+resource "vcd_vapp_vm" "web3" {
+  vapp_name     = var.vapp_name
+  name          = var.vm3_name
+  catalog_name  = var.catalog3_name
+  template_name = var.template3_name
+  memory        = var.vm3_memory
+  cpus          = var.vm3_cpu
+  cpu_cores     = var.vm3_cpu_core
+  cpu_hot_add_enabled=true
+  memory_hot_add_enabled=true
+  power_on=true
+
+    override_template_disk {
+    bus_type        = "paravirtual"
+    size_in_mb      = var.vm3_disk_size
+    bus_number      = 0
+    unit_number     = 0
+    iops            = 0
+  }
+
+
+  network {
+    type               = "org"
+    name               = var.org_network_name
+    ip_allocation_mode = "MANUAL"
+    ip                 = var.vm3_ip
+    adapter_type       = "VMXNET3"
+    is_primary         = true
+  }
+
+   customization{
+      enabled=false
+    }
+
+
+}
+
+
+resource "vcd_vapp_vm" "web4" {
+  vapp_name     = var.vapp_name
+  name          = var.vm4_name
+  catalog_name  = var.catalog4_name
+  template_name = var.template4_name
+  memory        = var.vm4_memory
+  cpus          = var.vm4_cpu
+  cpu_cores     = var.vm4_cpu_core
+  cpu_hot_add_enabled=true
+  memory_hot_add_enabled=true
+  power_on=true
+
+    override_template_disk {
+    bus_type        = "paravirtual"
+    size_in_mb      = var.vm4_disk_size
+    bus_number      = 0
+    unit_number     = 0
+    iops            = 0
+  }
+
+
+  network {
+    type               = "org"
+    name               = var.org_network_name
+    ip_allocation_mode = "MANUAL"
+    ip                 = var.vm4_ip
+    adapter_type       = "VMXNET3"
+    is_primary         = true
+  }
+
+   customization{
+      enabled=false
+    }
+
+
+}
+
+resource "vcd_vapp_vm" "web5" {
+  vapp_name     = var.vapp_name
+  name          = var.vm5_name
+  catalog_name  = var.catalog5_name
+  template_name = var.template5_name
+  memory        = var.vm5_memory
+  cpus          = var.vm5_cpu
+  cpu_cores     = var.vm5_cpu_core
+  cpu_hot_add_enabled=true
+  memory_hot_add_enabled=true
+  power_on=true
+
+    override_template_disk {
+    bus_type        = "paravirtual"
+    size_in_mb      = var.vm5_disk_size
+    bus_number      = 0
+    unit_number     = 0
+    iops            = 0
+  }
+
+
+  network {
+    type               = "org"
+    name               = var.org_network_name
+    ip_allocation_mode = "MANUAL"
+    ip                 = var.vm5_ip
+    adapter_type       = "VMXNET3"
+    is_primary         = true
+  }
+
+   customization{
+      enabled=false
+    }
+
+
+}
---
+++ variables.tf
@@ -0,0 +1,323 @@
+# vCloud Director Organization Variables
+variable "cd_username" {
+    type        = string
+    description = "vCD UserName"
+}
+variable "cd_password" {
+    type        = string
+    description = "vCD Password"
+}
+
+variable "cd_vdcadres" {
+    type        = string
+    description = "vCD Adresi"
+}
+
+variable "org_name" {
+    type        = string
+    description = "Organizasyon İsmi:"
+    default     = "list"
+}
+
+variable "vdc_name" {
+    type        = string
+    description = "VDC Adı:"
+    default     = "list"
+
+}
+
+
+variable "vapp_name" {
+    type        = string
+    description = "VAPP Adı:"
+    default     = "list"
+}
+
+
+variable "vm_name" {
+    type        = string
+    description = "Sunucu Adı:"
+}
+
+
+variable "vm_description" {
+    type        = string
+    description = "MANAGED/UNMANAGED:"
+    default     = "list"
+}
+
+variable "vm_cpu" {
+    type        = string
+    description = "CPU:"
+    default     = "list"
+}
+
+variable "vm_cpu_core" {
+    type        = string
+    description = "CPU Core:"
+    default     = "list"
+}
+
+
+variable "vm_memory" {
+    type        = string
+    description = "Memory:"
+    default     = "list"
+}
+
+variable "vm_disk_size" {
+    type        = string
+    description = "Disk:"
+    default     = "list"
+}
+
+
+variable "org_network_name" {
+    type        = string
+    description = "Network Adı:"
+    default     = "list"
+}
+    
+
+variable "catalog_name" {
+    type        = string
+    description = "Catalog:"
+    default     = "list"
+}
+
+variable "template_name" {
+  type        = string
+  description = "OS:"
+  default     = "list"
+}
+
+
+variable "vm_ip" {
+    type        = string
+    description = "IP Adresi:"
+}
+
+variable "vm2_name" {
+    type        = string
+    description = "Sunucu Adı:"
+}
+
+
+variable "vm2_description" {
+    type        = string
+    description = "MANAGED/UNMANAGED:"
+    default     = "list"
+}
+
+variable "vm2_cpu" {
+    type        = string
+    description = "CPU:"
+    default     = "list"
+}
+
+variable "vm2_cpu_core" {
+    type        = string
+    description = "CPU Core:"
+    default     = "list"
+}
+
+
+variable "vm2_memory" {
+    type        = string
+    description = "Memory:"
+    default     = "list"
+}
+
+variable "vm2_disk_size" {
+    type        = string
+    description = "Disk:"
+    default     = "list"
+}
+
+
+variable "catalog2_name" {
+    type        = string
+    description = "Catalog:"
+    default     = "list"
+}
+
+variable "template2_name" {
+  type        = string
+  description = "OS:"
+  default     = "list"
+}
+
+
+variable "vm2_ip" {
+    type        = string
+    description = "IP Adresi:"
+}
+
+variable "vm3_name" {
+    type        = string
+    description = "Sunucu Adı:"
+}
+
+
+variable "vm3_description" {
+    type        = string
+    description = "MANAGED/UNMANAGED:"
+    default     = "list"
+}
+
+variable "vm3_cpu" {
+    type        = string
+    description = "CPU:"
+    default     = "list"
+}
+
+variable "vm3_cpu_core" {
+    type        = string
+    description = "CPU Core:"
+    default     = "list"
+}
+
+
+variable "vm3_memory" {
+    type        = string
+    description = "Memory:"
+    default     = "list"
+}
+
+variable "vm3_disk_size" {
+    type        = string
+    description = "Disk:"
+    default     = "list"
+}
+
+
+variable "catalog3_name" {
+    type        = string
+    description = "Catalog:"
+    default     = "list"
+}
+
+variable "template3_name" {
+  type        = string
+  description = "OS:"
+  default     = "list"
+}
+
+
+variable "vm3_ip" {
+    type        = string
+    description = "IP Adresi:"
+}
+
+variable "vm4_name" {
+    type        = string
+    description = "Sunucu Adı:"
+}
+
+
+variable "vm4_description" {
+    type        = string
+    description = "MANAGED/UNMANAGED:"
+    default     = "list"
+}
+
+variable "vm4_cpu" {
+    type        = string
+    description = "CPU:"
+    default     = "list"
+}
+
+variable "vm4_cpu_core" {
+    type        = string
+    description = "CPU Core:"
+    default     = "list"
+}
+
+
+variable "vm4_memory" {
+    type        = string
+    description = "Memory:"
+    default     = "list"
+}
+
+variable "vm4_disk_size" {
+    type        = string
+    description = "Disk:"
+    default     = "list"
+}
+
+
+variable "catalog4_name" {
+    type        = string
+    description = "Catalog:"
+    default     = "list"
+}
+
+variable "template4_name" {
+  type        = string
+  description = "OS:"
+  default     = "list"
+}
+
+
+variable "vm4_ip" {
+    type        = string
+    description = "IP Adresi:"
+}
+
+
+variable "vm5_name" {
+    type        = string
+    description = "Sunucu Adı:"
+}
+
+
+variable "vm5_description" {
+    type        = string
+    description = "MANAGED/UNMANAGED:"
+    default     = "list"
+}
+
+variable "vm5_cpu" {
+    type        = string
+    description = "CPU:"
+    default     = "list"
+}
+
+variable "vm5_cpu_core" {
+    type        = string
+    description = "CPU Core:"
+    default     = "list"
+}
+
+
+variable "vm5_memory" {
+    type        = string
+    description = "Memory:"
+    default     = "list"
+}
+
+variable "vm5_disk_size" {
+    type        = string
+    description = "Disk:"
+    default     = "list"
+}
+
+
+variable "catalog5_name" {
+    type        = string
+    description = "Catalog:"
+    default     = "list"
+}
+
+variable "template5_name" {
+  type        = string
+  description = "OS:"
+  default     = "list"
+}
+
+
+variable "vm5_ip" {
+    type        = string
+    description = "IP Adresi:"
+}
