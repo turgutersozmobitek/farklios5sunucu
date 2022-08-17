3b7d2204de30b91c45edef0c71add16f8737e707
turgutersozmobitek <turgut.ersoz@mobitek.com>
2022-08-17 09:32:26 +0300

    Güncelleme #turgut
    Güncelleme #turgut

--- variables.tf
+++ variables.tf
@@ -1,16 +1,16 @@
 # vCloud Director Organization Variables
 variable "cd_username" {
     type        = string
-    description = "vCD UserName"
+    description = "vCD UserName*"
 }
 variable "cd_password" {
     type        = string
-    description = "vCD Password"
+    description = "vCD Password*"
 }
 
 variable "cd_vdcadres" {
     type        = string
-    description = "vCD Adresi"
+    description = "vCD Adresi*"
 }
 
 variable "org_name" {
@@ -21,26 +21,22 @@ variable "org_name" {
 
 variable "vdc_name" {
     type        = string
-    description = "VDC Adı:"
+    description = "VDC Adı:*"
     default     = "list"
-
 }
-
 
 variable "vapp_name" {
     type        = string
-    description = "VAPP Adı:"
+    description = "vAPP Adı:*"
     default     = "list"
 }
 
-
 variable "vm_name" {
     type        = string
-    description = "Sunucu Adı:"
+    description = "Sunucu Adı:*"
 }
 
 variable "computer_name" {
-    
     type        = string
     description = "Comp.Name:*"
 }
@@ -53,44 +49,44 @@ variable "vm_description" {
 
 variable "vm_cpu" {
     type        = string
-    description = "CPU:"
+    description = "CPU:*"
     default     = "list"
 }
 
 variable "vm_cpu_core" {
     type        = string
-    description = "CPU Core:"
+    description = "CPU Core:*"
     default     = "list"
 }
 
 
 variable "vm_memory" {
     type        = string
-    description = "Memory:"
+    description = "Memory:*"
     default     = "list"
 }
 
 variable "vm_disk_size" {
     type        = string
-    description = "Disk:"
+    description = "Disk:*"
     default     = "list"
 }
 
 variable "catalog_name" {
     type        = string
-    description = "Catalog:"
+    description = "Catalog:*"
     default     = "list"
 }
 
 variable "template_name" {
   type        = string
-  description = "OS:"
+  description = "OS:*"
   default     = "list"
 }
 
 variable "org_network_name" {
     type        = string
-    description = "Network Adı:"
+    description = "Network Adı:*"
     default     = "list"
 }
 
@@ -104,16 +100,15 @@ variable "adapter_type" {
 
 variable "vm_ip" {
     type        = string
-    description = "IP Adresi:"
+    description = "IP Adresi:*"
 }
 
 variable "vm2_name" {
     type        = string
-    description = "2.Sunucu Adı:"
+    description = "2.Sunucu Adı:*"
 }
 
 variable "computer_name2" {
-    
     type        = string
     description = "2.Comp.Name:*"
 }
@@ -127,55 +122,54 @@ variable "vm2_description" {
 
 variable "vm2_cpu" {
     type        = string
-    description = "CPU:"
+    description = "CPU:*"
     default     = "list"
 }
 
 variable "vm2_cpu_core" {
     type        = string
-    description = "CPU Core:"
+    description = "CPU Core:*"
     default     = "list"
 }
 
 
 variable "vm2_memory" {
     type        = string
-    description = "Memory:"
+    description = "Memory:*"
     default     = "list"
 }
 
 variable "vm2_disk_size" {
     type        = string
-    description = "Disk:"
+    description = "Disk:*"
     default     = "list"
 }
 
 
 variable "catalog2_name" {
     type        = string
-    description = "Catalog:"
+    description = "Catalog:*"
     default     = "list"
 }
 
 variable "template2_name" {
   type        = string
-  description = "OS:"
+  description = "OS:*"
   default     = "list"
 }
 
 
 variable "vm2_ip" {
     type        = string
-    description = "IP Adresi:"
+    description = "IP Adresi:*"
 }
 
 variable "vm3_name" {
     type        = string
-    description = "3.Sunucu Adı:"
+    description = "3.Sunucu Adı:*"
 }
 
 variable "computer_name3" {
-    
     type        = string
     description = "3.Comp.Name:*"
 }
@@ -188,55 +182,54 @@ variable "vm3_description" {
 
 variable "vm3_cpu" {
     type        = string
-    description = "CPU:"
+    description = "CPU:*"
     default     = "list"
 }
 
 variable "vm3_cpu_core" {
     type        = string
-    description = "CPU Core:"
+    description = "CPU Core:*"
     default     = "list"
 }
 
 
 variable "vm3_memory" {
     type        = string
-    description = "Memory:"
+    description = "Memory:*"
     default     = "list"
 }
 
 variable "vm3_disk_size" {
     type        = string
-    description = "Disk:"
+    description = "Disk:*"
     default     = "list"
 }
 
 
 variable "catalog3_name" {
     type        = string
-    description = "Catalog:"
+    description = "Catalog:*"
     default     = "list"
 }
 
 variable "template3_name" {
   type        = string
-  description = "OS:"
+  description = "OS:*"
   default     = "list"
 }
 
 
 variable "vm3_ip" {
     type        = string
-    description = "IP Adresi:"
+    description = "IP Adresi:*"
 }
 
 variable "vm4_name" {
     type        = string
-    description = "4.Sunucu Adı:"
+    description = "4.Sunucu Adı:*"
 }
 
 variable "computer_name4" {
-    
     type        = string
     description = "4.Comp.Name:*"
 }
@@ -249,56 +242,55 @@ variable "vm4_description" {
 
 variable "vm4_cpu" {
     type        = string
-    description = "CPU:"
+    description = "CPU:*"
     default     = "list"
 }
 
 variable "vm4_cpu_core" {
     type        = string
-    description = "CPU Core:"
+    description = "CPU Core:*"
     default     = "list"
 }
 
 
 variable "vm4_memory" {
     type        = string
-    description = "Memory:"
+    description = "Memory:*"
     default     = "list"
 }
 
 variable "vm4_disk_size" {
     type        = string
-    description = "Disk:"
+    description = "Disk:*"
     default     = "list"
 }
 
 
 variable "catalog4_name" {
     type        = string
-    description = "Catalog:"
+    description = "Catalog:*"
     default     = "list"
 }
 
 variable "template4_name" {
   type        = string
-  description = "OS:"
+  description = "OS:*"
   default     = "list"
 }
 
 
 variable "vm4_ip" {
     type        = string
-    description = "IP Adresi:"
+    description = "IP Adresi:*"
 }
 
 
 variable "vm5_name" {
     type        = string
-    description = "5.Sunucu Adı:"
+    description = "5.Sunucu Adı:*"
 }
 
 variable "computer_name5" {
-    
     type        = string
     description = "5.Comp.Name:*"
 }
@@ -312,44 +304,44 @@ variable "vm5_description" {
 
 variable "vm5_cpu" {
     type        = string
-    description = "CPU:"
+    description = "CPU:*"
     default     = "list"
 }
 
 variable "vm5_cpu_core" {
     type        = string
-    description = "CPU Core:"
+    description = "CPU Core:*"
     default     = "list"
 }
 
 
 variable "vm5_memory" {
     type        = string
-    description = "Memory:"
+    description = "Memory:*"
     default     = "list"
 }
 
 variable "vm5_disk_size" {
     type        = string
-    description = "Disk:"
+    description = "Disk:*"
     default     = "list"
 }
 
 
 variable "catalog5_name" {
     type        = string
-    description = "Catalog:"
+    description = "Catalog:*"
     default     = "list"
 }
 
 variable "template5_name" {
   type        = string
-  description = "OS:"
+  description = "OS:*"
   default     = "list"
 }
 
 
 variable "vm5_ip" {
     type        = string
-    description = "IP Adresi:"
+    description = "IP Adresi:*"
 }
